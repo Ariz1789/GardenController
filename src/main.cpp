70b94851eb805d7f7416d17b59f0247b5e5323e6
Francesco.Ariazzi <Francesco.Ariazzi@antaresvision.com>
2025-06-24 11:08:34 +0200

    added web service

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ void goIntoSpleep();
 void activateRelay();
 void print_wakeup_reason();
 void syncTimeWithNTP();
+void setWebServer();
 
 const int RELAY_PIN  = 8;
 
@@ -47,10 +48,17 @@ RTC_DATA_ATTR int bootCount = 0; // To track how many times the ESP32 has woken 
 RTC_DATA_ATTR bool morningActivation = false;
 RTC_DATA_ATTR bool eveningActivation = false;
 
+// Set web server port number to 80
+WiFiServer server(80);
+// Variable to store the HTTP request
+String header;
+
 
 void setup() {
 
   Serial.begin(9600);
+  delay(100); //wait for Serial to initialize
+  Serial.println("--- Setup Start ---");
 
   // Increment boot count every time the ESP32 starts (after reset or deep sleep wake)
   bootCount++;
@@ -72,6 +80,16 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT); // Set pin 8 (D8) as an output
   digitalWrite(RELAY_PIN, LOW); // Ensure relay is off initially
 
+  // start webservice
+  xTaskCreatePinnedToCore(
+      setWebServer, /* Function to implement the task */
+      "webservice", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task1,  /* Task handle. */
+      0); /* Core where the task should run */
+
   // activation bool reset
   eveningActivation = false;
   morningActivation = false;
@@ -81,6 +99,7 @@ void setup() {
   // This means pressing the button will wake the ESP32.
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_0, 0);
   Serial.println("Button wakeup enabled on GPIO0 (BOOT button).");
+  Serial.println("--- Setup End ---");
 }
 
 void loop() {
@@ -113,6 +132,12 @@ void loop() {
     eveningActivation = true;
     Serial.println("Within 10 PM activation window.");
     activateRelay();
+  }else if (currentHour == EVENING_H && currentMinute > 35 && morningActivation && eveningActivation) 
+  {
+    eveningActivation = false;
+    morningActivation = false;
+    Serial.println("Resetting activation flags after evening window.");
+    delay(1000); // Wait for 1 second before going to sleep
   }else{
     // wait for a minute to keep board on for some times
     delay(1 * 60 * 1000);
@@ -123,6 +148,10 @@ void loop() {
     }
     Serial.println("All attempts to update time done");
   }
+
+  Serial.println("--- Loop End ---");
+  
+  vTaskDelete(Task1); // Delete the web server task to free resources
 
   goIntoSpleep();
 }
@@ -226,4 +255,71 @@ void syncTimeWithNTP() {
     Serial.println("Failed to connect to WiFi hotspot.");
   }
   WiFi.mode(WIFI_OFF); // Turn off WiFi radio completely
+}
+
+void setWebServer(){
+WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+                        
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>Orto Ariazzi - Dotti Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>La Scheda si è attivata " + bootCount + " volte</p>");
+            client.println("<p>L'irrigazione della mattina è stata fatta: " + morningActivation?"si":"no" + "</p>");
+            client.println("<p>L'irrigazione della sera è stata fatta: " + eveningActivation?"si":"no" + "</p>");
+            
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
